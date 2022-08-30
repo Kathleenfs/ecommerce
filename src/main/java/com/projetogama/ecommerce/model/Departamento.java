6e83f95049520dd0f90863aaf204ce81907b6369
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-08-29 22:48:21 -0300

    criação model
    entidade departamento criada

---
+++ src/main/java/com/projetogama/ecommerce/model/Departamento.java
@@ -0,0 +1,46 @@
+package com.projetogama.ecommerce.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="departamento")
+public class Departamento {
+	
+	@Column(name="codigo") 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+	
+	@Column(name = "nome", length = 50, nullable = false)
+	private String nome;
+	
+	@Column(name = "descricao", nullable = true, columnDefinition = "TEXT")
+	private String descricao;
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+
+}
