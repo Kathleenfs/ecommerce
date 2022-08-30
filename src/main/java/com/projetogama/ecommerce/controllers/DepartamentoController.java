4e5c0274c4118caa15b9fdff85e76328401d7c8f
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-08-30 15:08:35 -0300

    .
    .

--- src/main/java/com/projetogama/ecommerce/controllers/DepartamentoController.java
+++ src/main/java/com/projetogama/ecommerce/controllers/DepartamentoController.java
@@ -3,25 +3,64 @@ package com.projetogama.ecommerce.controllers;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projetogama.ecommerce.dao.DepartamentoDAO;
 import com.projetogama.ecommerce.model.Departamento;
+import com.projetogama.ecommerce.service.IDepartamentoService;
 
 @RestController
 public class DepartamentoController {
 	
 	@Autowired
-	private DepartamentoDAO dao;
+	private IDepartamentoService service;
 	
 	@GetMapping("/departamentos")
 	public ArrayList<Departamento> recuperarTodos(){
-		ArrayList<Departamento> lista;
-		lista = (ArrayList<Departamento>)dao.findAll(); // realiza uma conversão de findAll para uma lista
-		return lista;
+		return service.buscarTodos();
 		}
 	
+	
+	@PostMapping("/departamentos")
+	public ResponseEntity<Departamento> incluirNovo(@RequestBody Departamento novo){//O objeto response Entity me da uma resposta htttp onde eu coloco codigo ou mensagem
+		Departamento res = service.criarNovo(novo);
+		if(res != null) {
+			return ResponseEntity.ok(res);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
+	@PutMapping("/departamentos")
+	public ResponseEntity<Departamento> alterar(@RequestBody Departamento dados){
+		Departamento res = service.atualizarDados(dados);
+		if(res != null) {
+			return ResponseEntity.ok(res);
+		}
+		return ResponseEntity.badRequest().build();
+		
+	}
+	
+	@DeleteMapping("/departamento/{id}")
+	public ResponseEntity<Departamento> excluirDepartamento(@PathVariable Integer id){
+		service.excluirDepartamento(id);
+		return ResponseEntity.ok(null);
+	}
+	
+	@GetMapping("/departamento/{codigo}")
+	public ResponseEntity<Departamento> buscarPeloId(@PathVariable Integer codigo){
+		Departamento res = service.buscarPeloId(codigo);
+		if(res != null) {
+			return ResponseEntity.ok(res);
+			
+		}
+		return ResponseEntity.notFound().build();
+	}
 	
 
 }
--- src/main/java/com/projetogama/ecommerce/service/DepartamentoServiceImpl.java
+++ src/main/java/com/projetogama/ecommerce/service/DepartamentoServiceImpl.java
@@ -2,37 +2,48 @@ package com.projetogama.ecommerce.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetogama.ecommerce.dao.DepartamentoDAO;
 import com.projetogama.ecommerce.model.Departamento;
 
+@Service
 public class DepartamentoServiceImpl implements IDepartamentoService{
 
+	@Autowired
+	private DepartamentoDAO dao;
+	
 	@Override
 	public Departamento criarNovo(Departamento novo) {
-		// TODO Auto-generated method stub
+		if(novo.getNome()!= null) {
+			return dao.save(novo);
+		}
 		return null;
 	}
 
 	@Override
 	public Departamento atualizarDados(Departamento dados) {
-		// TODO Auto-generated method stub
+		if(dados.getCodigo()!= null && dados.getNome()!= null) {
+			return dao.save(dados);
+		}
 		return null;
 	}
 
 	@Override
 	public ArrayList<Departamento> buscarTodos() {
 		// TODO Auto-generated method stub
-		return null;
+		return (ArrayList<Departamento>)dao.findAll();
 	}
 
 	@Override
 	public Departamento buscarPeloId(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.findById(id).orElse(null);
 	}
 
 	@Override
 	public void excluirDepartamento(Integer id) {
-		// TODO Auto-generated method stub
+		dao.deleteById(id);
 		
 	}
 
