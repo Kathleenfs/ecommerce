a3a7a821d8ee80349c9755f94accd888ef8c38fa
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-09-04 18:49:55 -0300

    queries

--- src/main/java/com/projetogama/ecommerce/dao/ProdutoDAO.java
+++ src/main/java/com/projetogama/ecommerce/dao/ProdutoDAO.java
@@ -1,9 +1,17 @@
 package com.projetogama.ecommerce.dao;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.projetogama.ecommerce.model.Produto;
 
 public interface ProdutoDAO extends CrudRepository<Produto, Integer> {
+
+	//queries
+	
+	public ArrayList<Produto> findByOrderPreco();
+	public ArrayList<Produto> findByOrderPrecoDesc();
+	public ArrayList<Produto> findByOrderNomeContaining(String palavraChave);;
 
 }
