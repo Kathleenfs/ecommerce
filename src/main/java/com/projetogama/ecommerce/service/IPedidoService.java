ef2a41843c8a32c8ddf7e759819485b044911376
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-09-04 17:15:35 -0300

    interface pedido

---
+++ src/main/java/com/projetogama/ecommerce/service/IPedidoService.java
@@ -0,0 +1,11 @@
+package com.projetogama.ecommerce.service;
+
+import com.projetogama.ecommerce.model.Pedido;
+
+public interface IPedidoService {
+	
+	public Pedido inserirNovo(Pedido novo);
+	
+	public Pedido recuperarPeloNumero(Integer numero);
+
+}
