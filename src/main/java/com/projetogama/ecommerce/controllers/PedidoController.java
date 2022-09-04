130f95822901bb22c48d302a34bb4015fa8798de
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-09-04 17:13:11 -0300

    criado pedidoController

--- src/main/java/com/projetogama/ecommerce/controllers/PedidoController.java
+++ src/main/java/com/projetogama/ecommerce/controllers/PedidoController.java
@@ -1,5 +1,25 @@
 package com.projetogama.ecommerce.controllers;
 
-public class PedidoController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.projetogama.ecommerce.model.Pedido;
+
+@Controller
+public class PedidoController {
+	
+	@GetMapping("/pedidos/{numero}")
+	public ResponseEntity<Pedido> recuperarPeloNumero(@PathVariable Integer numero){
+		return null;
+	}
+	
+	@PostMapping("/pedidos")
+	public ResponseEntity<Pedido> inserirNovoPedido(@RequestBody Pedido novo){
+		return null;
+		
+	}
 }
